6738bdb514ff994a70acbc0b89c8485edf694bee
Artescern <124739786+Artescern@users.noreply.github.com>
2024-06-01 12:28:59 -0400

    Add files via upload
    Added the pagination feature

---
+++ src/components/UserContext.js
@@ -0,0 +1,15 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [userInfo, setUserInfo] = useState(null);
+
+  return (
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
--- src/firebase.js
+++ src/firebase.js
@@ -35,4 +35,3 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 
 export{app, auth};
-
